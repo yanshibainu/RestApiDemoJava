e82e3d4c810afaf3af065e6f8b2fedd9edac2222
Mao <xckdum@gmail.com>
2021-11-12 15:46:08 +0800

    UserService複寫update加入防呆

--- src/main/java/tw/com/service/UserService.java
+++ src/main/java/tw/com/service/UserService.java
@@ -12,5 +12,23 @@ public class UserService extends AbstractService<User>{
 		user.setId(map.size());
 		return super.create(user);
 	}
+
+	@Override
+	public User update(int id, User user) {
+		if(map.get(id).getId() != user.getId() ) {
+			user.setId(map.get(id).getId());
+		}
+		if(user.getName() == null) {
+			user.setName(map.get(id).getName());
+		}
+		if(user.getEmail() == null) {
+			user.setEmail(map.get(id).getEmail());
+		}
+		if(user.getPassword() == null) {
+			user.setPassword(map.get(id).getPassword());
+		}
+		
+		return super.update(id, user);
+	}
 	
 }
